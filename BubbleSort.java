dd7c436d21f66cb7968c195db34952479b751e3c
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2025-07-21 15:34:48 +0530

    Bubble Sort

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,4 +1,6 @@
 public class BubbleSort {
+
+    //printing array
     public static void PrintArr(int arr[]){
         for(int i=0; i<arr.length; i++){
             System.out.print(arr[i]+" ");
@@ -6,9 +8,11 @@ public class BubbleSort {
         System.out.println();
     }
 
+    
     public static void main(String args[]){
         int arr[] = {7,8,3,1,2};
 
+        //sorting
         for(int i=0; i<arr.length; i++){
             for(int j=i+1; j<arr.length; j++){
                 if(arr[i]>arr[j]){
