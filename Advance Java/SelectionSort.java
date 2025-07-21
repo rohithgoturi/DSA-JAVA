8a30d365155ab0a22ce3eb2f342eb1990adeefce
Rohith Satish Goturi <rohithgoturi28@gmail.com>
2025-07-21 16:18:44 +0530

    Selection Sort

--- Advance Java/SelectionSort.java
+++ Advance Java/SelectionSort.java
@@ -1,7 +1,33 @@
 public class SelectionSort {
 
     //array printing function
+    public static void PrintArr(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
     public static void main(String args[]){
+        int arr[] = {7,8,3,1,2};
+
+        //sorting
+        for(int i=0; i<arr.length; i++){
+            int smallest = i;
+
+            //finding smallest element
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[j]<arr[smallest]){
+                    smallest = j;
+                }
+            }
+
+            //swapping
+            int temp = arr[smallest];
+            arr[smallest] = arr[i];
+            arr[i] = temp;
+        }
+
+        PrintArr(arr);
 
     }
 }
